08e78420c295eadb51eca4ecefd35c6186293bf0
Buka Cakrawala <kaka10garuda@gmail.com>
2017-01-26 21:33:55 -0800

    produce a sentence from dict/words & completed

--- onlineCourses/dictionary_words.py
+++ onlineCourses/dictionary_words.py
@@ -1,23 +1,24 @@
 import random
 import sys
-import os
-
 import subprocess
 
 
 def check_sys_argument():
     number = int(sys.argv[1])
     dictionary_words = []
-    # call_result = os.system('tail -%s /usr/share/dict/words' % (number))
-    # return "RESULT: %s" % (call_result)
-    # call_result = os.popen('tail -%s /usr/share/dict/words' % (number)).read()
-    # return "This is call result: %s " % (call_result)
+    final_words = []
+    # pass in the command_prompt to access the words
     proc = subprocess.Popen('tail -%s /usr/share/dict/words' % (str(number)),
                             shell=True, stdout=subprocess.PIPE)
+    # set the output of the terminal
     output = proc.communicate()[0]
     dictionary_words.append(output)
     for word in dictionary_words:
-        return word.split()
+        final_words = word.split()
+    # rearrange the elemnt within the final words array
+    random.shuffle(final_words)
+    # return all elements to join into a sentence
+    return " ".join(final_words)
 
 
 if __name__ == '__main__':

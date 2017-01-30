9d412a4f1267d1c60c14c192c5d0cb9d58586857
Buka Cakrawala <kaka10garuda@gmail.com>
2017-01-29 16:40:08 -0800

    generate random word and its probability

--- onlineCourses/gen_histogram.py
+++ onlineCourses/gen_histogram.py
@@ -1,4 +1,5 @@
-import statistics
+import random
+import sys
 
 
 # this function takes in string file and returns
@@ -10,7 +11,7 @@ def histogram(source_text):
 
     with open(source_text, 'r') as myfile:
         # read the data file and remove all unnecessary characters
-        data = myfile.read().replace('\n', '').replace('.', '').replace(',', '').replace('(', '').replace(')', '')
+        data = myfile.read().replace('\n', '').replace('.', ' ').replace(',', '').replace('(', '').replace(')', '').replace('"','').lower()
         # split the data from sentences to words.
         words_array = data.split()
     # HOW MANY DIFFERENT WORDS ARE USED?
@@ -24,12 +25,21 @@ def unique_words(dict_histogram):
     most_used_word = max(dict_histogram, key=dict_histogram.get)
     # THE LEAST WORDS IN THE TEXT, THAT ONLY CAME OUT ONCE
     least_words = [k for k, v in dict_histogram.iteritems() if v == 1]
-    # returns the total count of uniqe words in the histogram
+    # returns the total count of unique words in the histogram
     return len(least_words)
-
 
 def frequency(word, dict_histogram):
     return dict_histogram[word]
+
+
+def generate_word(dict_histogram):
+    random_word = random.choice(dict_histogram.keys())
+    print generate_probability(random_word, dict_histogram)
+
+
+def generate_probability(random_word, dict_histogram):
+    return "%s => %s"%(random_word, float(frequency(random_word, dict_histogram)) / float(sum(dict_histogram.values())))
+
 
 def test():
     lst = []
@@ -37,6 +47,7 @@ def test():
         lst.append(i)
 
 if __name__ == '__main__':
-    import timeit
+
+    generate_word(histogram(sys.argv[1]))
     # print unique_words(histogram('paul_blog.txt'))
-    print timeit.timeit("test()", setup="from __main__ import test")
+    # print timeit.timeit("test()", setup="from __main__ import test")

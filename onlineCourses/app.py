961813d1f1fe0362a5cfa07413d270886a83960e
Buka Cakrawala <kaka10garuda@gmail.com>
2017-02-03 08:29:23 -0800

    host

--- onlineCourses/app.py
+++ onlineCourses/app.py
@@ -23,4 +23,5 @@ def hello_world():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    host = '0.0.0.0'
+    app.run(debug=True, host=host, port=port)

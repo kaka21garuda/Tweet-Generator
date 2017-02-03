3948778e5a4895ed17347b6af8d5599d7a78f15b
Buka Cakrawala <kaka10garuda@gmail.com>
2017-02-02 23:25:22 -0800

    port update

--- onlineCourses/app.py
+++ onlineCourses/app.py
@@ -24,4 +24,4 @@ def hello_world():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', posrt=port)
+    app.run(debug=True, host='0.0.0.0', port=port)

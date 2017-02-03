5799c82197edd88863226a759b01372eee34c503
Buka Cakrawala <kaka10garuda@gmail.com>
2017-02-02 23:01:55 -0800

    update proc(2)

--- onlineCourses/app.py
+++ onlineCourses/app.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 from flask import Flask
 from gen_histogram import histogram
@@ -22,4 +23,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', posrt=port)

9afede2b8f595a3b5d74d59416a649ea66f204f6
Buka Cakrawala <kaka10garuda@gmail.com>
2017-02-08 10:55:21 -0800

    try generate sentence (1)

--- onlineCourses/app.py
+++ onlineCourses/app.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 
 def histogram_probability(dict_histogram):
     prob_dict = {}
+    sum_values = sum(dict_histogram.values())
     for k in dict_histogram.keys():
-        prob_dict[k] = (float(dict_histogram[k]) / sum(dict_histogram.values()))
+        prob_dict[k] = (float(dict_histogram[k]) / sum_values)
     # return stochastic_pick(prob_dict)
     return prob_dict
 
@@ -28,14 +29,15 @@ def stochastic_pick(dict_histogram):
 
 @app.route('/')
 def hello_world():
-    print "hello world"
+    sentence = []
     dict_histogram = histogram('tom_sawyer.txt')
     tes_dict = {'one': 1, 'blue': 1, 'two': 1, 'fish': 4, 'red': 1}
     dict_prob = histogram_probability(dict_histogram)
-    return stochastic_pick(dict_prob)
+    for i in range(17):
+        sentence.append(stochastic_pick(dict_prob))
+    return " ".join(sentence)
 
 
 if __name__ == '__main__':
-    print "Main Section"
     app.run(debug=True)
     # print stochastic_pick()
